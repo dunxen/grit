6ca8b4d0198b1af874431795ded33bb31b88f43e
Duncan Dean <duncangleeddean@gmail.com>
2020-04-10 14:13:58 +0200

    Refactor init into function

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,36 @@ fn main() {
 
     if let Some(command) = args.next() {
         match &command[..] {
-            "init" => {
-                let path = args
-                    .next()
-                    .map(|s| Ok(PathBuf::from(s)))
-                    .unwrap_or(env::current_dir());
-
-                match path {
-                    Ok(root_path) => {
-                        let git_path = root_path.join(".git");
-
-                        for dir in &["objects", "refs"] {
-                            let dir_path = git_path.join(dir);
-                            let res = fs::create_dir_all(&dir_path);
-
-                            if let Err(error) = res {
-                                panic!("Could not create {:?}: {:?}", dir_path, error)
-                            }
-                        }
-
-                        println!(
-                            "Initialized empty git repository in {}",
-                            git_path.to_str().unwrap_or("directory")
-                        );
-                    }
-                    Err(error) => panic!("Error forming path: {:?}", error),
-                }
-            }
+            "init" => init(&mut args),
             _ => unimplemented!(),
         }
+    }
+}
+
+fn init(args: &mut std::iter::Skip<env::Args>) {
+    let path = args
+        .next()
+        .map(|s| Ok(PathBuf::from(s)))
+        .unwrap_or(env::current_dir());
+
+    match path {
+        Ok(root_path) => {
+            let git_path = root_path.join(".git");
+
+            for dir in &["objects", "refs"] {
+                let dir_path = git_path.join(dir);
+                let res = fs::create_dir_all(&dir_path);
+
+                if let Err(error) = res {
+                    panic!("Could not create {:?}: {:?}", dir_path, error)
+                }
+            }
+
+            println!(
+                "Initialized empty git repository in {}",
+                git_path.to_str().unwrap_or("directory")
+            );
+        }
+        Err(error) => panic!("Error forming path: {:?}", error),
     }
 }
